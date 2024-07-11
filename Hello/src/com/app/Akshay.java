e4cf94f5dc33e5fe5598c6b0726b003a16cd3a81
Administrator <Administrator@192.168.100.10>
2024-07-11 14:27:49 +0530

    B1 commit

--- Hello/src/com/app/Akshay.java
+++ Hello/src/com/app/Akshay.java
@@ -3,7 +3,8 @@ package com.app;
 public class Akshay {
 
 	public static void main(String[] args) {
-		System.out.println("Akshay"); 
+		System.out.println("Akshay");
+		System.out.println("Jaiswal");
 
 	}
 
